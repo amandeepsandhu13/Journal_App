4ffb1d88751c2ab75a4b378a6588d6c284a9cdda
amandeep <samandeepkaue34@gmail.com>
2024-11-21 19:08:57 -0500

    added transaction manager

--- src/main/java/org/aman/journalapp/controller/JournalEntryController.java
+++ src/main/java/org/aman/journalapp/controller/JournalEntryController.java
@@ -8,6 +8,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -38,6 +39,7 @@ public class JournalEntryController {
         }
     }
 
+    @Transactional
     @PostMapping("{userName}")
     public ResponseEntity<JournalEntry> createJournal(@RequestBody JournalEntry newEntry, @PathVariable String userName){
         try{
